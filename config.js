380fcc88502884b1ee1dbc499fd5acd50193c001
yashilas <yashilas.2009@gmail.com>
2026-08-08 13:38:03 -0400

    Added URL

---
+++ config.js
@@ -0,0 +1,6 @@
+// GlideRise front-end configuration.
+// Change the API URL here to point the chat widget at your backend.
+
+window.GLIDERISE_CONFIG = {
+  apiUrl: "https://bladeguard-2erc.onrender.com"
+};
